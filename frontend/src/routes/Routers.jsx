d803aed133c86cec977e4ec9124319def4c69bd3
mohamedkhaledmk <mohamed181371@feng.bu.edu.eg>
2024-04-02 12:55:16 +0200

    starter files

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Contact from "../pages/Contact";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+    </Routes>
+  );
+};
+
+export default Routers;
